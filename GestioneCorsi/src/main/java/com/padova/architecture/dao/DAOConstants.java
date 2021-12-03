63af92aa6425c67c73e569c6a49d5cf71e8326e3
Elia <elia.rech@studenti.unipd.it>
2021-12-03 17:46:13 +0100

    Aggiornato daoconstants

--- GestioneCorsi/src/main/java/com/padova/architecture/dao/DAOConstants.java
+++ GestioneCorsi/src/main/java/com/padova/architecture/dao/DAOConstants.java
@@ -10,18 +10,20 @@ public interface DAOConstants {
 	String DELETE_CORSI = "delete from corsi where codcorso = ?";
 	String SELECT_CORSI_BY_ID = "select * from corsi where codcorso = ?";
 	String SELECT_DATAINIZIOCORSO = "select max(datainiziocorso) from corsi";
-
+	String SELECT_MEDIACORSI = "select avg(to_date(datafinecorso, 'YYYY-MM-DD') - to_date(datainiziocorso, 'YYYY-MM-DD')) from corsi";
+	
 	//--------CORSICORSISTI--------
 	String SELECT_CORSO_CORSISTA_BY_ID = "select * from corsocorsista where IDCORSO=?";
 	String SELECT_CORSO_CORSISTA = "Select * from corsocorsista";
 	String DELETE_CORSO_CORSISTA = "delete from corsocorsista where IDCORSI=?";
 	String INSERT_CORSO_CORSISTA = "Insert into corsocorsista values(?,?,?)";
-	
+	String SELECT_MAXCORSI ="select corsi.nomecorso, count(*) from corsi, corsocorsista, corsisti where corsi.codcorso = corsocorsista.idcorsi and corsocorsista.idcorsisti = corsisti.codcorsista group by corsi.nomecorso having count(*) = (select max(count(*)) from corsi, corsisti, corsocorsista where corsi.codcorso = corsocorsista.idcorsi and corsocorsista.idcorsisti = codcorsista group by corsi.nomecorso)";
+
 	//--------CORSISTI--------
 	String SELECT_CORSISTI = "Select * from corsisti";
 	String SELECT_CORSISTI_BY_ID = "Select * from corsisti where codcorsista = ?";
 	String DELETE_CORSISTI = "Delete from corsisti where codcorsista = ?";
-
+	String SELECT_CORSISTI_TOT = "select count(*) from corsisti";
 	
 	//--------AMMINISTRATORI--------
 	String SELECT_ADMINPASS = "Select codadmin from amministratore where nomeadmin = ?";
