67ee0e298e2c84d66217f21903020bf01196acdb
Elia <elia.rech@studenti.unipd.it>
2021-12-03 12:03:25 +0100

    Modificato DAOCONSTANTS

--- GestioneCorsi/src/main/java/com/padova/architecture/dao/DAOConstants.java
+++ GestioneCorsi/src/main/java/com/padova/architecture/dao/DAOConstants.java
@@ -6,15 +6,19 @@ public interface DAOConstants {
 	String SELECT_DOCENTI = "Select * from docenti";
 	
 	//--------CORSI--------
-	
-	
+	String SELECT_DATAINIZIOCORSO = "select max(datainiziocorso) from corsi";
+
 	//--------CORSICORSISTI--------
-	
+	String SELECT_CORSO_CORSISTA_BY_ID = "select * from corsocorsista where IDCORSO=?";
+	String SELECT_CORSO_CORSISTA = "Select * from corsocorsista";
+	String DELETE_CORSO_CORSISTA = "delete from corsocorsista where IDCORSI=?";
+	String INSERT_CORSO_CORSISTA = "Insert into corsocorsista values(?,?,?)";
 	
 	//--------CORSISTI--------
 	String SELECT_CORSISTI = "Select * from corsisti";
 	String SELECT_CORSISTI_BY_ID = "Select * from corsisti where codcorsista = ?";
 	String DELETE_CORSISTI = "Delete from corsisti where codcorsista = ?";
+
 	
 	//--------AMMINISTRATORI--------
 	
@@ -26,3 +30,5 @@ public interface DAOConstants {
 	String SELECT_CORSISTA_SEQ = "Select corsisti_seq.nextval from dual";
 	String SELECT_CORSO_SEQ = "Select corsi_seq.nextval from dual";
 }
+
+
